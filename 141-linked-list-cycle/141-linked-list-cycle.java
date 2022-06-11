043933cf1fbdb4d994e633ac7997316fdfaf3b17
Maggie S <106038403+Maggie885@users.noreply.github.com>
2022-06-11 15:13:52 -0700

    Time: 0 ms (100.00%), Space: 46 MB (40.78%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,17 @@
+class Solution{
+    //Time: O(n)
+    //Space: O(1)
+    public boolean hasCycle(ListNode head){
+        if(head == null) return false;
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while(slow != fast){
+            if(fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
